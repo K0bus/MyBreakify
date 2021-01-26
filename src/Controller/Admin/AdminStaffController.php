3a92f25fed47ce24a529175a0d042fdf09049eb6
K0bus <contact@k0bus.fr>
2021-01-26 19:20:30 +0100

    Staff design

---
+++ src/Controller/Admin/AdminStaffController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Entity\UserBreak;
+use App\Form\AdminUserBreakType;
+use App\Form\UserBreakType;
+use DateInterval;
+use DateTime;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints\Date;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin/staff", name="app_admin_staff")
+     */
+    public function dashboard(): Response
+    {
+        return $this->render('admin/staff.html.twig');
+    }
+
+}
