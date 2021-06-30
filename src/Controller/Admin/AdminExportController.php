2d3da2c5ef200f951d741720cb8dfc02ba4cf7d3
K0bus <contact@k0bus.fr>
2021-06-30 16:47:19 +0200

    Add break export

--- src/Controller/Admin/AdminExportController.php
+++ src/Controller/Admin/AdminExportController.php
@@ -25,9 +25,44 @@ class AdminExportController extends AbstractController
         $errors = array();
         array_push($errors, "Export actuellement non disponible.");
 
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $exportRequestType = $request->query->get('type');
+        if(isset($exportRequestType) && $exportRequestType != NULL)
+        {
+            $start = DateTime::createFromFormat("d/m/Y",$request->query->get('filter_date_start'));
+            $end = DateTime::createFromFormat("d/m/Y", $request->query->get('filter_date_end'));
+            if($exportRequestType == "breaks")
+            {
+                $breaks = $this->getDoctrine()
+                    ->getManager()
+                    ->createQuery('SELECT b FROM App\Entity\UserBreak b WHERE b.date >= :start AND b.date <= :end')
+                    ->setParameter('start', $start)
+                    ->setParameter('end', $end)
+                    ->getResult();
+                header('Content-Type: text/csv; charset=utf-8');
+                header('Content-Disposition: attachment; filename="FooBarFileName_' . date('Ymd') . '.csv"');
+                header("Pragma: no-cache");
+                header("Expires: 0");
+                $this->outputCSV($breaks);
+            }
+        }
+
         $this->denyAccessUnlessGranted('ROLE_N1');
         return $this->render('admin/export.html.twig', [
             'errors' => $errors
         ]);
+    }
+
+    public function outputCSV($data, $useKeysForHeaderRow = true) {
+        if ($useKeysForHeaderRow) {
+            array_unshift($data, array_keys(reset($data)));
+        }
+    
+        $outputBuffer = fopen("php://output", 'w');
+        foreach($data as $v) {
+            fputcsv($outputBuffer, $v);
+        }
+        fclose($outputBuffer);
     }
 }
