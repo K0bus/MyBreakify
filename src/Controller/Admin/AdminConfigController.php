5a66ea5e842b6495d6069385c42387eb07ad7f30
K0bus <contact@k0bus.fr>
2021-07-05 11:39:00 +0200

    Add encoder

--- src/Controller/Admin/AdminConfigController.php
+++ src/Controller/Admin/AdminConfigController.php
@@ -80,7 +80,7 @@ class AdminConfigController extends AbstractController
                 else
                 {
                     foreach ($data as $key => $v) {
-                        $this->updateUser($v);
+                        $this->updateUser($v, $encoder);
                     }
                 }
             }
@@ -111,7 +111,7 @@ class AdminConfigController extends AbstractController
         return $this->redirectToRoute("app_admin_recovery");
     }
 
-    public function updateUser($v)
+    public function updateUser($v, $encoder)
     {
         $t = $this->getDoctrine()
         ->getRepository(User::class)
