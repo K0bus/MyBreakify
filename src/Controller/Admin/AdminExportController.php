b02ca7a6e0012961696174c25eab60d55827f5f0
K0bus <contact@k0bus.fr>
2021-07-09 16:37:31 +0200

    Simplify code

--- src/Controller/Admin/AdminExportController.php
+++ src/Controller/Admin/AdminExportController.php
@@ -33,23 +33,12 @@ class AdminExportController extends AbstractController
             $end = DateTime::createFromFormat("d/m/Y", $request->request->get('filter_date_end'))->setTime(23, 59, 59);
             if($exportRequestType == "recovery")
             {
-                if($start == $end)
-                {
-                    $breaks = $this->getDoctrine()
-                    ->getRepository(UserRecovery::class)
-                    ->findBy([
-                        "date" => $start
-                    ]);
-                }
-                else
-                {
-                    $recoveries = $this->getDoctrine()
+                $recoveries = $this->getDoctrine()
                     ->getManager()
                     ->createQuery('SELECT b FROM App\Entity\UserRecovery b WHERE b.date >= :start AND b.date <= :end')
                     ->setParameter('start', $start)
                     ->setParameter('end', $end)
                     ->getResult();
-                }
                 $rows = array();
                 $data = array("id", "date", "username", "time_from", "time_to", "status", "comment", "requested_at");
                 $rows[] = implode(';', $data);
@@ -78,41 +67,30 @@ class AdminExportController extends AbstractController
             }
             if($exportRequestType == "breaks")
             {
-                if($start == $end)
-                {
-                    $breaks = $this->getDoctrine()
-                    ->getRepository(UserBreak::class)
-                    ->findBy([
-                        "date" => $start
-                    ]);
-                }
-                else
-                {
-                    $breaks = $this->getDoctrine()
-                        ->getManager()
-                        ->createQuery('SELECT b FROM App\Entity\UserBreak b WHERE b.date >= :start AND b.date <= :end')
-                        ->setParameter('start', $start)
-                        ->setParameter('end', $end)
-                        ->getResult();
-                }
-                    $rows = array();
-                    $data = array("id", "date", "username", "time", "requested_at");
+                $breaks = $this->getDoctrine()
+                    ->getManager()
+                    ->createQuery('SELECT b FROM App\Entity\UserBreak b WHERE b.date >= :start AND b.date <= :end')
+                    ->setParameter('start', $start)
+                    ->setParameter('end', $end)
+                    ->getResult();
+                $rows = array();
+                $data = array("id", "date", "username", "time", "requested_at");
+                $rows[] = implode(';', $data);
+                foreach ($breaks as $break) {
+                    $data = array($break->getId(), $break->getDate()->format('d/m/Y'), $break->getUserId()->getUsername(), $break->getTime()->format('H:i'), $break->getRequestedAt()->format('d/m/Y H:i'));
                     $rows[] = implode(';', $data);
-                    foreach ($breaks as $break) {
-                        $data = array($break->getId(), $break->getDate()->format('d/m/Y'), $break->getUserId()->getUsername(), $break->getTime()->format('H:i'), $break->getRequestedAt()->format('d/m/Y H:i'));
-                        $rows[] = implode(';', $data);
-                    }
-                    if(count($rows) > 1)
-                    {
-                        $content = implode("\n", $rows);
-                        $response = new Response($content);
-                        $response->headers->set('Content-Type', 'text/csv;');
-                        $response->headers->set('Content-Disposition', 'attachment; filename='.basename('break_data_'.$start->format('dmY').'_'.$end->format('dmY').'.csv'));
-                        return $response;
-                    }
-                    else {
-                        array_push($errors, "Aucune données à extraire pour la période du ".$start->format('d/m/Y')." au ".$end->format('d/m/Y'));
-                    }
+                }
+                if(count($rows) > 1)
+                {
+                    $content = implode("\n", $rows);
+                    $response = new Response($content);
+                    $response->headers->set('Content-Type', 'text/csv;');
+                    $response->headers->set('Content-Disposition', 'attachment; filename='.basename('break_data_'.$start->format('dmY').'_'.$end->format('dmY').'.csv'));
+                    return $response;
+                }
+                else {
+                    array_push($errors, "Aucune données à extraire pour la période du ".$start->format('d/m/Y')." au ".$end->format('d/m/Y'));
+                }
             }
 
         }
