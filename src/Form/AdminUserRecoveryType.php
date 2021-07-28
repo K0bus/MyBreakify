7b8c81fa1eff07d707fb3f56552b3c74e476c9e6
K0bus <contact@k0bus.fr>
2021-07-28 14:24:51 +0200

    Change class name

--- src/Form/AdminUserRecoveryType.php
+++ src/Form/AdminUserRecoveryType.php
@@ -8,7 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class AdminUserBreakType extends AbstractType
+class AdminUserRecoveryType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
