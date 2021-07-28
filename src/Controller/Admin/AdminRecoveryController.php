9a192c8ea4e5a4b930846ae247fd2e8b1a2752b6
K0bus <contact@k0bus.fr>
2021-07-28 10:54:36 +0200

    Fix date format

--- src/Controller/Admin/AdminRecoveryController.php
+++ src/Controller/Admin/AdminRecoveryController.php
@@ -33,7 +33,7 @@ class AdminRecoveryController extends AbstractController
 
         if($request->request->get('filter_date') != null)
         {
-            $date = DateTime::createFromFormat("d/M/y", $request->request->get('filter_date'));
+            $date = DateTime::createFromFormat("d/m/Y", $request->request->get('filter_date'));
             $fixed_date = $date;
         }  
 
@@ -41,6 +41,8 @@ class AdminRecoveryController extends AbstractController
         ->getRepository(UserRecovery::class)
         ->findBy([
             "date" => $date,
+        ], [
+            "time_to" => "ASC",
         ]);
         $users = array();
         foreach ($recoveries as $key => $value) {
